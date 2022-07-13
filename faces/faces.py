b0ecffb6d731fefe1afe164a7c0882bdcf6105d1
N3rg <moren.g.couch@gmail.com>
2022-07-13 08:08:34 +0000

    Wed, Jul 13, 2022, 8:08 AM +00:00

---
+++ faces/faces.py
@@ -0,0 +1,18 @@
+def main():
+    user_input = str(input("Please input something with :) or :( "))
+    convert(user_input)
+
+
+def convert(user_input):
+    splitted_input = user_input.split(" ")
+    emojis = {
+        ":)": "😀",
+        ":(": "😞"
+    }
+    outcome = " "
+    for word in splitted_input:
+        outcome += emojis.get(word, word) + " "
+    print(outcome)
+
+
+main()
