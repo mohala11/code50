355fafac7fe158e1c477213b5fcc95f4f6990334
N3rg <moren.g.couch@gmail.com>
2022-07-15 07:28:33 +0000

    Fri, Jul 15, 2022, 7:28 AM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,6 @@
-question = str.lower.strip(input("What is the Answer to the Great Question of Life, the Universe, and Everything? "))
+question = str.lower(input("What is the Answer to the Great Question of Life, the Universe, and Everything? "))
 x = ["42", "forty-two", "forty two"]
+question = question.strip()
 if question in x:
     print("Yes!")
 else:
