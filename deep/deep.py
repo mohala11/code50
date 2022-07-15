8bcb99ceec59f7a440397ad7d394277eb78dae9d
N3rg <moren.g.couch@gmail.com>
2022-07-15 07:14:55 +0000

    Fri, Jul 15, 2022, 7:14 AM +00:00

---
+++ deep/deep.py
@@ -0,0 +1,6 @@
+question = str(input("What is the Answer to the Great Question of Life, the Universe, and Everything? "))
+x = ["42", "forty-two", "forty two"]
+if question in x:
+    print("Yes!")
+else:
+    print("No!")
