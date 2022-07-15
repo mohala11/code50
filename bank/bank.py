5ab8749f07a69a475d3d7dc75b25a888ef7e9130
N3rg <moren.g.couch@gmail.com>
2022-07-15 08:57:06 +0000

    Fri, Jul 15, 2022, 8:57 AM +00:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,8 +1,8 @@
 text = "Greeting: "
 greeting = str.lower(input(text).strip())
 if greeting.startswith("hello") == True:
-    print("0$")
+    print("$0")
 elif greeting.startswith("h") == True:
-    print("20$")
+    print("$20")
 else:
-    print("100$")
+    print("$100")
