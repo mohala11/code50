9b047751c798ceebf1ba48cca06177b8d26a2b2c
N3rg <moren.g.couch@gmail.com>
2022-07-18 11:39:02 +0000

    Mon, Jul 18, 2022, 11:39 AM +00:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,3 +1,7 @@
-text = "Expression: "
-userinput = input(text).split(" ")
-print(userinput)
+def main():
+    text = "Expression: "
+    userinput = input(text).split(" ")
+    x = float(userinput[0])
+    y = userinput[1]
+    z = float(userinput[2])
+    resu
