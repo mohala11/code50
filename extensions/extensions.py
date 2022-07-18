a18bc035d27edb911227710bd4424a19569766b4
N3rg <moren.g.couch@gmail.com>
2022-07-18 11:13:33 +0000

    Mon, Jul 18, 2022, 11:13 AM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,3 +1,25 @@
-x = "File name: "
-userinput = str(input(x)).split(".")
-print()
+def main():
+    text = "File name: "
+    file_name = str.lower(input(text).strip())
+    result = check(file_name)
+    print(result)
+
+
+def check(file_name):
+    if file_name.endswith(".gif"):
+        return "image/gif"
+    elif file_name.endswith(".jpg", ".jpeg"):
+        return "image/jpeg"
+    elif file_name.endswith(".png"):
+        return "image/png"
+    elif file_name.endswith(".pdf"):
+        return "application/pdf"
+    elif file_name.endswith(".txt"):
+        return "text/plain"
+    elif file_name.endswith(".zip"):
+        return "application/zip"
+    else:
+        return "application/octet-stream"
+
+
+main()
