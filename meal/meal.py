82ecabf0142a585f3897fc41b0a7abb0f6fc78af
N3rg <moren.g.couch@gmail.com>
2022-07-19 15:00:16 +0000

    Tue, Jul 19, 2022, 3:00 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -7,7 +7,7 @@ def main():
     result = convert(time)
     if result >= 7.0 and result <= 8.0 and jopka == "a.m.":
         print("breakfast time")
-    elif result >= 12.0 and result <= 12.(9) and jopka == "a.m.":
+    elif result >= 12.0 and result <= 12.99 and jopka == "a.m.":
         print("lunch time")
     elif result == 1 and jopka == "p.m.":
         print("lunch time")
