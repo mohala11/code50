f5479e4572c1ae4a0a40db9bc8d03ff979c68dfe
N3rg <moren.g.couch@gmail.com>
2022-07-19 14:11:23 +0000

    Tue, Jul 19, 2022, 2:11 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -2,12 +2,23 @@ def main():
     text = "What time is it? "
     time = str(input(text))
     result = convert(time)
-        if result
+        if result >= 7.0 and <= 8.0:
+            print("breakfast time")
+        elif result >= 12.0 and <= 13.0:
+            print("meal time")
+        elif result >= 18.0 and <= 19.0:
+            print("dinner time")
+        else:
+            print()
+
 
 
 def convert(time):
     hours, minutes = time.split(":")
-    
+    floatminutes = minutes / 60
+    floattime = hours + floatminutes
+    return floattime
+
 
 
 if __name__ == "__main__":
