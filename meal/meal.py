96da263b8f06f387f1dee9f23f7230ecefa555d5
N3rg <moren.g.couch@gmail.com>
2022-07-22 12:31:32 +0000

    Fri, Jul 22, 2022, 12:31 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -9,7 +9,7 @@ def main():
         print("breakfast time")
     elif result >= 12.0 and result <= 12.99 and is_am_time:
         print("lunch time")
-    elif result == 1 and jopka == "p.m.":
+    elif result == 1 and !is_am_time:
         print("lunch time")
     elif result >= 6.0 and result <= 7.0 and !is_am_time:
         print("dinner time")
