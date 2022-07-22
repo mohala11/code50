e4feb04a69a52e0b8b971306322fbc82916a67b2
N3rg <moren.g.couch@gmail.com>
2022-07-22 13:23:59 +0000

    Fri, Jul 22, 2022, 1:23 PM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,21 +1,20 @@
 def main():
     text = "CamelCase: "
     user_input = str(input(text))
-    s = user_input.split()
 #    print(s)
     convert(user_input)
 
 
 def convert(user_input):
     s = user_input.split()
-        for i in s:
-            if i.isupper:
-                i = "_" + i.lower()
-                return i
-            else:
-                return i
-    print(i)
-
+    outcome = " "
+    for i in s:
+        if i.isupper:
+            i = "_" + i.lower()
+            outcome += i
+        else:
+            outcome += i
+    print(outcome)
 
 
 main()
