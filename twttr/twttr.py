ab6962be51f9a21ed8412cc36d4c7fa2807f5403
N3rg <moren.g.couch@gmail.com>
2022-07-23 14:37:12 +0000

    Sat, Jul 23, 2022, 2:37 PM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -4,8 +4,15 @@ def main():
     convert(user_input)
 
 
-def conver(user_input):
+def convert(user_input):
     s = list(user_input)
     outcome = 'Output: '
     list_of_vowels = ['E', 'Y', 'U', 'I', 'O', 'A', 'e', 'y', 'u', 'i', 'o', 'a']
+    for i in s:
+        if i in list_of_vowels:
+            outcome = outcome
+        elif i not in list_of_vowels:
+            outcome += i
+    print(outcome)
     
+
