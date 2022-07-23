5b64f64947a140114ab4a1c298e60b467160c264
N3rg <moren.g.couch@gmail.com>
2022-07-23 14:20:39 +0000

    Sat, Jul 23, 2022, 2:20 PM +00:00

---
+++ twttr/twttr.py
@@ -0,0 +1,11 @@
+def main():
+    text = 'Input: '
+    user_input = str(input(text))
+    convert(user_input)
+
+
+def conver(user_input):
+    s = list(user_input)
+    vowels = {
+        
+    }
