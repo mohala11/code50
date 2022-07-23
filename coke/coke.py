b3a85a9d867a78477c3678a85013254dd6dedad0
N3rg <moren.g.couch@gmail.com>
2022-07-23 14:06:19 +0000

    Sat, Jul 23, 2022, 2:06 PM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -2,11 +2,11 @@ def main():
     price = 50
     coinbase = [5, 10, 25]
     while price > 0:
-        s = int(input('Amount Due: ' + str(price) + '\nInsert Coin: '))
+        s = int(input(f'Amount Due: {price} \nInsert Coin: '))
         if s in coinbase:
             price = price - s
         else:
-            break
+            print('input valid coin')
     print('Change Owed:', abs(price))
 
 
