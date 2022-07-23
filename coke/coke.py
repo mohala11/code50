450d2b7d09f8f615f53143f3e5a41a2befde308e
N3rg <moren.g.couch@gmail.com>
2022-07-23 13:31:00 +0000

    Sat, Jul 23, 2022, 1:31 PM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,3 +1,10 @@
 def main():
+    price = '50'
     coinbase = ['5', '10', '25']
-    s = int(input(Amount Due:'))
+    s = int(input('Amount Due: ' + price + '\nInsert Coin: '))
+    while s in coinbase and price >= 0:
+        price += s;
+        print('Amount Due: ' + price + '\nInsert Coin: ')
+
+
+main()
