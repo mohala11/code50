c6d9e86dd2a1c1c982cb66c4988c79463bf921ce
N3rg <moren.g.couch@gmail.com>
2022-07-25 11:49:41 +0000

    Mon, Jul 25, 2022, 11:49 AM +00:00

--- plates.py/plates.py
+++ plates.py/plates.py
@@ -1,12 +1,19 @@
 def main():
     plate = input("Plate: ")
-    if is_valid(plate):
-        print("Valid")
-    else:
+    x = len(plate)
+    if x <= 1 or x > 6:
         print("Invalid")
+    if x >= 2 and x <= 6:
+        
 
 
-def is_valid(s):
+#    if is_valid(plate):
+#        print("Valid")
+#   else:
+#        print("Invalid")
+
+
+#def is_valid(s):
 
 
 
