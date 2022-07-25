38cb75a1d34a6873e612a3d12039a9530a1b326f
N3rg <moren.g.couch@gmail.com>
2022-07-25 17:56:24 +0000

    Mon, Jul 25, 2022, 5:56 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -9,13 +9,7 @@ def main():
 def is_valid(s):
     marks = [",", " ", ".", "/", "*", "_", "-", ";", ":"]
     numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    for i in s and i not in marks:
-        if i[1] and i[2] in numbers:
-            break
-        elif i[3:].isdigit() and i[3] == 0:
-            break
-        else:
-            return s
+    
 
 
 main()
