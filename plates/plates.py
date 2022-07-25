858016da1ce40817377415689606a887740b648d
N3rg <moren.g.couch@gmail.com>
2022-07-25 17:15:02 +0000

    Mon, Jul 25, 2022, 5:15 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -10,10 +10,10 @@ def is_valid(plate):
     marks = [",", " ", ".", "/", "*", "_", "-", ";", ":"]
     numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     s = list(plate)
-    for i in s and i not in marks:
-        if i[1], i[2] in numbers:
+    for i in s and i not in marks and len(s) <= 6:
+        if i[0] and i[1] in numbers:
             break
-        else if 
+        elif i[]
 
 
 
