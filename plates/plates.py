85baddeefa6c6d3df72a5bb6f1979816961cabbb
N3rg <moren.g.couch@gmail.com>
2022-07-25 17:07:59 +0000

    Mon, Jul 25, 2022, 5:07 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,6 +1,5 @@
 def main():
     plate = input("Plate: ")
-
     if is_valid(plate):
         print("Valid")
     else:
@@ -10,6 +9,7 @@ def main():
 def is_valid(s):
     marks = [",", " ", ".", "/", "*", "_", "-", ";", ":"]
     numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    s = 
+    s = list(plate)
+
 
 main()
