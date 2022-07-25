a66a8666cd922bd8654906962285351eee7cee8a
N3rg <moren.g.couch@gmail.com>
2022-07-25 17:09:45 +0000

    Mon, Jul 25, 2022, 5:09 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -6,10 +6,13 @@ def main():
         print("Invalid")
 
 
-def is_valid(s):
+def is_valid(plate):
     marks = [",", " ", ".", "/", "*", "_", "-", ";", ":"]
     numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     s = list(plate)
+    while i in s and i not in numbers:
+        
+
 
 
 main()
