5155f26e8c73a23737ee6107fc1d6535694b5f03
N3rg <moren.g.couch@gmail.com>
2022-07-25 17:36:35 +0000

    Mon, Jul 25, 2022, 5:36 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -12,7 +12,14 @@ def is_valid(s):
     while len(s) <=6 and s not in marks:
         if s[0] and s[1] in numbers:
             break
-        
+        elif s[2] and s[3] and s[4] and s[5] in numbers and s[2] == numbers[1]:
+            break
+        elif s[3] and s[4] and s[5] in numbers and s[3] == numbers[1]:
+            break
+        elif s[4] and s[5] in numbers and s[4] == numbers[1]:
+            break
+        elif s[5] in numbers and s[s] == numbers[1]:
+            break
         else:
             return s
 
