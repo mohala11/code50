509424c2fd12e5c37d640fda1e4708cdd723bc32
N3rg <moren.g.couch@gmail.com>
2022-07-25 12:07:11 +0000

    Mon, Jul 25, 2022, 12:07 PM +00:00

--- plates.py/plates.py
+++ plates.py/plates.py
@@ -4,32 +4,21 @@ def main():
     if x != 4 or x != 6:
         print("Invalid")
     else:
-        is_valid()
+        print("loh")
+        #is_valid()
 
 
-def is_valid(plate):
-    if len(x) == 4:
-        if type(plate[0:2]) == "str" and type(plate[3:4]) == "int":
-            print("Valid")
-        else:
-            print("Invalid")
-    elif len(x) == 6:
-        if type(plate[0:3]) == "str" and type(plate[4:6]) == "int":
-            print("Valid")
-        else:
-            print("Invalid")
-
-
-
-
-
-#    if is_valid(plate):
-#        print("Valid")
-#   else:
-#        print("Invalid")
-
-
-#def is_valid(s):
+#def is_valid(plate):
+#    if len(x) == 4:
+#        if type(plate[0:2]) == "class 'str'" and type(plate[3:4]) == "class 'int'":
+#            print("Valid")
+#        else:
+#            print("Invalid")
+#    elif len(x) == 6:
+#        if type(plate[0:3]) == "class 'int'" and type(plate[4:6]) == "class 'int'":
+#            print("Valid")
+#        else:
+#            print("Invalid")
 
 
 
