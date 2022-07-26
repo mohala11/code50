069c49f6c68de364e762a5fa163f37fa791e864d
N3rg <moren.g.couch@gmail.com>
2022-07-26 11:34:59 +0000

    Tue, Jul 26, 2022, 11:34 AM +00:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,3 +1,18 @@
 d = {
-    "apple": 
+    "apple": "130",
+    "avocado": "50",
+    "banana": "110",
+    "cantaloupe": "50",
+    "grapefruit": "60",
+    "grapes": "90",
+    "honeydew melon": "50",
+    "kiwifuit": "90",
+    "lemon": "15",
+    "lime": "20",
+    "nectarine": "60",
+    "orange": "80",
+    "peach": "60",
+    "pear": "100",
+    
+
 }
