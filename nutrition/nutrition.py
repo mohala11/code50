b4745a34f7a822b2f8743dd2c175b605bd13bc37
N3rg <moren.g.couch@gmail.com>
2022-07-26 11:38:55 +0000

    Tue, Jul 26, 2022, 11:38 AM +00:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -13,6 +13,14 @@ d = {
     "orange": "80",
     "peach": "60",
     "pear": "100",
-    
-
+    "pineapple": "50",
+    "plums": "70",
+    "strawberries": "50",
+    "sweet cherries": "100",
+    "tangerine": "50",
+    "watermelon": "80"
 }
+x = "Item: "
+k = str.lower(input(x))
+for k in d:
+    
