6f08e881e87ee695a3ab7c44aa26c948c1966e23
N3rg <moren.g.couch@gmail.com>
2022-07-27 13:37:47 +0000

    Wed, Jul 27, 2022, 1:37 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,11 +1,19 @@
-while True:
-    try:
-        fraction = input("Fraction: ")
+def main():
+    fraction = input("Fraction: ")
+    x = count(fraction)
+    print(f"{x} %")
+
+
+def count(fraction):
+    while True:
         x = int(fraction[0])
         y = int(fraction[2])
-        if x > y:
-            raise ValueError("zaebaL")
-    except ValueError:
-        pass
-    percent = x / y * 100
+        try:
+            i = x / y *100
+            if x > y:
+                raise ValueError("zaebaL")
+        except ValueError:
+            pass
+            percent = x / y * 100
+
     print(f"{percent}%")
