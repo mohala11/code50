aa7ae9ee35ed45965bd2e3c7216c3a50334bfbb8
N3rg <moren.g.couch@gmail.com>
2022-07-29 12:46:27 +0000

    Fri, Jul 29, 2022, 12:46 PM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -2,7 +2,7 @@ date = str(input("Date: "))
 x = date[-5]
 if date[-5] == "/":
     date = date.split("/")
-    date[0], date[1] = float(date[0]), float(date[1])
-    print(f"{date[2]}-{date[0]:.2f}-{date[1]:2f}")
+    date[0], date[1] = int(date[0]), int(date[1])
+    print(f"{date[2]}-{date[0]:02}-{date[1]:02}")
 else:
     print("fok uuuuuuuuuuuuuuuuuuu")
