d3c1a488e9474b84891dcb97d7213f752b05db69
N3rg <moren.g.couch@gmail.com>
2022-07-29 08:15:17 +0000

    Fri, Jul 29, 2022, 8:15 AM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,12 +1,14 @@
 grocery_list = {}
 while True:
     try:
-        item = str.upper("")
+        item = str.upper(input())
         if item not in grocery_list:
             i = 1
             grocery_list[item] = i
+            print(grocery_list)
         elif item in grocery_list:
             grocery_list[item] += 1
+            print(grocery_list)
     except EOFError:
         print(grocery_list)
         break
