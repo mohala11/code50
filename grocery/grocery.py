2fcdabfd27e52ba4d41ef0fb554352c32bd0ea48
N3rg <moren.g.couch@gmail.com>
2022-07-29 09:17:18 +0000

    Fri, Jul 29, 2022, 9:17 AM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -18,7 +18,7 @@ while True:
     # если юзер прерывает цикл
     except EOFError:
         # сортируем словарь по айтемам
-        {k: v for k, v in sorted(grocery_list.items(), key=lambda item: item[1])}
+        {k: v for k, v in sorted(grocery_list.items(), key=lambda item: item[0])}
         # выводим словарь на экран в виде КОЛИЧЕСТВО АЙТЕМ
         for key, value in grocery_list.items():
             print(value, key, sep = " ")
