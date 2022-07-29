126c31d26f5989f018c9689c04b708ffdcb8cefd
N3rg <moren.g.couch@gmail.com>
2022-07-29 13:34:07 +0000

    Fri, Jul 29, 2022, 1:34 PM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -13,26 +13,35 @@ d = [
     "December"
 ]
 date = str(input("Date: "))
+# сторим переменную для определения формата даты
 x = date[-5]
 while True:
     try:
+        # если дата формата MM/DD/YYYY
         if date[-5] == "/":
             date = date.split("/")
             date[0], date[1] = int(date[0]), int(date[1])
             if date[0] > 12:
-                print("dolbaeb1")
+                raise ValueError
             elif date[1] > 31:
-                print("dolbaeb2")
+                raise ValueError
             elif len(date[2]) > 4:
-                print("dolbaeb3")
+                raise ValueError
             else:
                 print(f"{date[2]}-{date[0]:02}-{date[1]:02}")
+        # если дата формата MONTHTITLE DD, YYYY
         elif date[-5] == " ":
             date = date.replace(",","")
             date = date.split(" ")
             date[1] = int(date[1])
             month = d.index(date[0])+1
-            if date[0] in d:
-                print(f"{date[2]}-{month:02}-{date[1]:02}")
+            if date[0] not in d:
+                raise ValueError
+            elif date[1] > 31:
+                raise ValueError
+            elif elif len(date[2]) > 4:
+                raise ValueError
             else:
-                print("huy sosi")
+                print(f"{date[2]}-{month:02}-{date[1]:02}")
+    except ValueError:
+        pass
