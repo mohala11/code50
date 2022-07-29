0d2f0a44f894e6bd3cfa1e3efe66e17afdf1b65f
N3rg <moren.g.couch@gmail.com>
2022-07-29 08:59:44 +0000

    Fri, Jul 29, 2022, 8:59 AM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,12 +1,25 @@
+# заводим словарь
 grocery_list = {}
+# цикл наполнения
 while True:
     try:
+        # запрашиваем айтем
         item = str.upper(input())
+        # если айтем новый
         if item not in grocery_list:
+            # стартовое количество айтема
             i = 1
+            # запись айтема и количества айтема в словарь
             grocery_list[item] = i
+            # если айтем уже в словаре
         elif item in grocery_list:
+            # увеличивает количество айтема на 1
             grocery_list[item] += 1
+    # если юзер прерывает цикл
     except EOFError:
-        print(item, value, {k: v for k, v in sorted(grocery_list.items(), key=lambda item: item[1])})
+        # сортируем словарь по айтемам
+        {k: v for k, v in sorted(grocery_list.items(), key=lambda item: item[1])}
+        # выводим словарь на экран в виде КОЛИЧЕСТВО АЙТЕМ
+        for key, value in grocery_list.items():
+            print(value, key, sep = " ")
         break
