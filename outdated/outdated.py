809644bdc8e19c8c1aa0536d9125f609d3784692
N3rg <moren.g.couch@gmail.com>
2022-07-29 12:50:29 +0000

    Fri, Jul 29, 2022, 12:50 PM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -2,7 +2,10 @@ date = str(input("Date: "))
 x = date[-5]
 if date[-5] == "/":
     date = date.split("/")
+elif len(date[0]) > 2 and date[0] > 12:
+    print("dolbaeb")
     date[0], date[1] = int(date[0]), int(date[1])
     print(f"{date[2]}-{date[0]:02}-{date[1]:02}")
+
 else:
     print("fok uuuuuuuuuuuuuuuuuuu")
