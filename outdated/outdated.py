87b65a6a3caffda89d10dd9aaef68a015ffc8bd4
N3rg <moren.g.couch@gmail.com>
2022-07-31 07:49:52 +0000

    Sun, Jul 31, 2022, 7:49 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -33,25 +33,37 @@ while True:
                 break
         # если дата формата MONTHTITLE DD, YYYY
         elif date[-5] == " ":
+            date = date.split(" ")
             # проверяем строку на наличие запятой после даты
             coma = re.compile(',')
             if(coma.search(date) == None):
                 raise ValueError
             else:
-                date = date.replace(",", "")
-                date = date.split(" ")
-                date[1] = int(date[1])
-                if date[0] not in d:
-                    raise ValueError
-                elif date[1] > 31:
-                    raise ValueError
-                elif len(date[2]) > 4:
-                    raise ValueError
-                else:
-                    month = d.index(date[0])+1
-                    print(f"{date[2]}-{month:02}-{date[1]:02}")
-                    break
+                date[1] = date[1].replace(",", "")
+            date[1] = int(date[1])
+            if date[0] not in d:
+                raise ValueError
+            elif date[1] > 31:
+                raise ValueError
+            elif len(date[2]) > 4:
+                raise ValueError
+            else:
+                month = d.index(date[0])+1
+                print(f"{date[2]}-{month:02}-{date[1]:02}")
+                break
         else:
             raise ValueError
     except ValueError:
         pass
+
+
+
+
+
+
+
+
+
+
+
+
