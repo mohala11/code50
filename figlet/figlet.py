9dde1645a1d3b88f625aece3db516716960960c7
N3rg <moren.g.couch@gmail.com>
2022-08-03 10:56:11 +0000

    Wed, Aug 3, 2022, 10:56 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,3 +1,18 @@
+# Among the fonts supported by FIGlet are those at figlet.org/examples.html.
+
+# FIGlet has since been ported to Python as a module called pyfiglet.
+
+# In a file called figlet.py, implement a program that:
+
+#    Expects zero or two command-line arguments:
+#        Zero if the user would like to output text in a random font.
+#        Two if the user would like to output text in a specific font, in which case the first of the two should be -f or --font, and the second of the two should be the name of the font.
+#    Prompts the user for a str of text.
+#    Outputs that text in the desired font.
+
+# If the user provides two command-line arguments and the first is not -f or --font or the second is not the name of a font, the program should exit via sys.exit with an error message.
+
+
 import sys
 from pyfiglet import Figlet
 import random
@@ -5,7 +20,9 @@ import random
 figlet = Figlet()
 list = figlet.getFonts()
 
+
 def main(figlet, list):
+    # определяем вид инпута и его корректность
     a = check(list)
     if a == 1:
         rng(list, figlet)
@@ -13,16 +30,6 @@ def main(figlet, list):
         specificfont(figlet)
     elif a == 3:
         sys.exit("Invalid usage")
-#    if len(sys.argv) == 1 or len(sys.argv) == 3 and sys.argv[1] == "-f" or sys.argv[1] == "--font" and sys.argv[2] in list:
-#       x = str(input("Input: "))
-#        if len(sys.argv) == 1:
-#            rng(x, list, figlet)
-#        elif sys.argv[1] == "-f" or sys.argv[1] == "--font" and sys.argv[2] in list:
-#            specificfont(x, figlet)
-#        else:
-#            sys.exit("Invalid usage")
-#    else:
-#        sys.exit("Invalid usage")
 
 
 def check(list):
