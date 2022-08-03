f7447345fe89db76e9ae2dc3755ae7879d227ac8
N3rg <moren.g.couch@gmail.com>
2022-08-03 10:15:20 +0000

    Wed, Aug 3, 2022, 10:15 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -23,11 +23,11 @@ def rng(x):
 def specificfont(x):
     figlet = Figlet()
     list = figlet.getFonts()
-    if sys.argv[1] == "-f" or sys.argv[1] == "--font" and sys.argv[2] in list:
+    if sys.argv[1] != "-f" or sys.argv[1] != "--font" and sys.argv[2] not in list:
+        sys.exit("Invalid usage")
+    else:
         figlet.setFont(font=sys.argv[2])
         print(figlet.renderText(x))
-    else:
-        sys.exit("Invalid usage")
 
 
 main()
