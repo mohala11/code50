cd6955a44a6b4c758a1d637258b6aa5623652a05
N3rg <moren.g.couch@gmail.com>
2022-08-03 09:49:33 +0000

    Wed, Aug 3, 2022, 9:49 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,13 +1,14 @@
 import sys
 from pyfiglet import Figlet
+import random
 
 
 def main():
-    x = str.input("Input ")
-    if sys.argv[1] == None:
-        random(x)
-    else:
-        specificfont(x)
+    x = str(input("Input "))
+#    if sys.argv[1] == None:
+    random(x)
+#    else:
+#        specificfont(x)
 
 
 def random(x):
