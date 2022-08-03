90cc4d0cfa6d29c5c2db414d29522a69216ddde7
N3rg <moren.g.couch@gmail.com>
2022-08-03 10:18:35 +0000

    Wed, Aug 3, 2022, 10:18 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,25 +4,23 @@ import random
 
 def main():
     x = str(input("Input: "))
+    figlet = Figlet()
+    list = figlet.getFonts()
     if len(sys.argv) == 1:
         rng(x)
-    if len(sys.argv) == 3:
+    elif sys.argv[1] != "-f" or sys.argv[1] != "--font" and sys.argv[2] not in list:
         specificfont(x)
     else:
         sys.exit("Invalid usage")
 
 
 def rng(x):
-    figlet = Figlet()
-    list = figlet.getFonts()
     font = random.choice(list)
     figlet.setFont(font=font)
     print(figlet.renderText(x))
 
 
 def specificfont(x):
-    figlet = Figlet()
-    list = figlet.getFonts()
     if sys.argv[1] != "-f" or sys.argv[1] != "--font" and sys.argv[2] not in list:
         sys.exit("Invalid usage")
     else:
