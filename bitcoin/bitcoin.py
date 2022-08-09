8d0f0f8d86f2a400db17136a33f8e46af148ee95
N3rg <moren.g.couch@gmail.com>
2022-08-09 13:37:08 +0000

    Tue, Aug 9, 2022, 1:37 PM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -13,5 +13,5 @@ except ValueError:
 r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 
 o = r.json()
-for result in o[""]:
-    print(result[])
+for result in o[]:
+    print(result)
