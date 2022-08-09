4c8f87a7ed0b567072f8c58d6356e19bba63619c
N3rg <moren.g.couch@gmail.com>
2022-08-09 13:07:15 +0000

    Tue, Aug 9, 2022, 1:07 PM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -6,11 +6,15 @@ try:
     y = float(sys.argv[1])
     print(y)
 except ValueError:
-    sys.exit("dolbaeb")
+    sys.exit("error")
 
 
-response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+headers = requests.headers("https://api.coindesk.com/v1/bpi/currentprice.json")
 
-o = response.json()
-for result in o["USD"]:
-    print(result["rate_float"])
+print(headers)
+
+# response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+
+# o = response.json()
+# for result in o["USD"]:
+#    print(result["rate_float"])
