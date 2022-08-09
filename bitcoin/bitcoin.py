28daa8a2936278a644b5e662a4141c3779628699
N3rg <moren.g.couch@gmail.com>
2022-08-09 11:29:55 +0000

    Tue, Aug 9, 2022, 11:29 AM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,8 +2,12 @@ import json
 import requests
 import sys
 
-x = float(sys.argv)
-if 
+
+try:
+    x = float(sys.argv)
+except ValueError:
+    sys.exit("dolbaeb")
+
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
 # print(json.dumps(response.json(), indent=2))
