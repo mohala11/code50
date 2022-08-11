f518a3a74c464517b4b3e98f750b1f8fa4a91b90
N3rg <moren.g.couch@gmail.com>
2022-08-11 14:18:01 +0000

    ddd, Aug 8/11/2022, YYYY, 2:18 A +0

--- twttr/test_twttr.py
+++ twttr/test_twttr.py
@@ -5,4 +5,11 @@ def convert_only_letters():
     assert convert("my name is Vitaly") == "m nm s Vtly"
     assert convert("my wife's name in Nataly") == "m wf's nm s Ntly"
 
-def converts_letters_and_numbers()
+
+def converts_letters_and_numbers():
+    assert convert("Boing 747") == "Bng 747"
+    assert convert("I am 30 years old") == "m 30 yrs ld"
+
+
+def convert_only_numbers():
+    assert convert("412874187941") == "412874187941"
