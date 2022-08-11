355e1afe9885554d8ef48e20243abf36ef579654
N3rg <moren.g.couch@gmail.com>
2022-08-11 14:14:46 +0000

    ddd, Aug 8/11/2022, YYYY, 2:14 A +0

--- twttr/test_twttr.py
+++ twttr/test_twttr.py
@@ -1,4 +1,8 @@
 from twttr import convert
 
 
-def 
+def convert_only_letters():
+    assert convert("my name is Vitaly") == "m nm s Vtly"
+    assert convert("my wife's name in Nataly") == "m wf's nm s Ntly"
+
+def converts_letters_and_numbers()
