910ce27e6a6cda792fcf675fcb8caa24a09710f0
N3rg <moren.g.couch@gmail.com>
2022-08-11 07:19:18 +0000

    Thu, Aug 11, 2022, 7:19 AM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -9,7 +9,7 @@ def convert(user_input):
     outcome = ''
     list_of_vowels = ['e', 'u', 'i', 'o', 'a']
     for i in s:
-        if i.lowercase() in list_of_vowels:
+        if i.lower() in list_of_vowels:
             outcome = outcome
         elif i not in list_of_vowels:
             outcome += i
