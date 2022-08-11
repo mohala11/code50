1a8c8bb7d70735b74acfdf97a6c44897078f4dc3
N3rg <moren.g.couch@gmail.com>
2022-08-11 14:30:23 +0000

    ddd, Aug 8/11/2022, YYYY, 2:30 A +0

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,15 +1,15 @@
 from twttr import convert
 
 
-def convert_only_letters():
-    assert convert("my name is Vitaly") == "m nm s Vtly"
-    assert convert("my wife's name in Nataly") == "m wf's nm s Ntly"
+def test_convert_only_letters():
+    assert convert("my name is Vitaly") == "my nm s Vtly"
+    assert convert("my wife's name is Nataly") == "my wf's nm s Ntly"
 
 
-def converts_letters_and_numbers():
+def test_converts_letters_and_numbers():
     assert convert("Boing 747") == "Bng 747"
-    assert convert("I am 30 years old") == "m 30 yrs ld"
+    assert convert("I am 30 years old") == " m 30 yrs ld"
 
 
-def convert_only_numbers():
+def test_convert_only_numbers():
     assert convert("412874187941") == "412874187941"
