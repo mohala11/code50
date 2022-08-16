32b2f1ace4ad5feac532562b564845c109679d7a
N3rg <moren.g.couch@gmail.com>
2022-08-16 14:41:14 +0000

    ddd, Aug 8/16/2022, YYYY, 2:41 A +0

--- bank/test_bank.py
+++ bank/test_bank.py
@@ -2,9 +2,9 @@ from bank import value
 
 
 def test_value_hello():
-    assert value("hello") == "$0"
-    assert value("HELLO") == "$0"
-    assert value("Hello, i'm David") == "$0"
+    assert value("hello") == 0
+    assert value("HELLO") == 0
+    assert value("Hello, i'm David") == 0
 
 
 def test_value_h():
