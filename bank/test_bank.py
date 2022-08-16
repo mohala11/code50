7f80bba6bb1bf7f02064acbcdcf208fc2c4798b2
N3rg <moren.g.couch@gmail.com>
2022-08-16 14:35:36 +0000

    ddd, Aug 8/16/2022, YYYY, 2:35 A +0

--- bank/test_bank.py
+++ bank/test_bank.py
@@ -1,4 +1,10 @@
 from bank import value
 
 
-def test
+def test_value_hello():
+    assert value("hello") == "$0"
+    assert value("HELLO") == "$0"
+    assert value("Hello, i'm David") == "$0"
+
+def test_value_h():
+    assert value 
