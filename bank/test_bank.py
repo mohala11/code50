00c4fbfdf3b227127476106be637fd71c039a557
N3rg <moren.g.couch@gmail.com>
2022-08-16 14:40:02 +0000

    ddd, Aug 8/16/2022, YYYY, 2:40 A +0

--- bank/test_bank.py
+++ bank/test_bank.py
@@ -6,5 +6,14 @@ def test_value_hello():
     assert value("HELLO") == "$0"
     assert value("Hello, i'm David") == "$0"
 
+
 def test_value_h():
-    assert value 
+    assert value("Hi") == "$20"
+    assert value("Hihao") == "$20"
+    assert value("Hallo, dude") == "$20"
+
+
+def test_value_any():
+    assert value("74287419784") == "$100"
+    assert value("Nihao") == "$100"
+    assert value("shdhfakjfja") == "$100"
