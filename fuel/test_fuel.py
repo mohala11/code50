92100be191839ff6ae66042c8c62df7ed3890789
N3rg <moren.g.couch@gmail.com>
2022-08-17 10:57:55 +0000

    Wed, Aug 17, 2022, 10:57 AM +00:00

--- fuel/test_fuel.py
+++ fuel/test_fuel.py
@@ -2,6 +2,14 @@ import pytest
 from fuel import convert, gauge
 
 
-def test_letters():
+def test_value():
     with pytest.raises(ValueError):
         convert("cat/dog")
+
+
+def test_zero():
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
+
+
+def 
