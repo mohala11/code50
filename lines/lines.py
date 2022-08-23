0506efb74ede369a6686a9f20a26c23c61249fb7
N3rg <moren.g.couch@gmail.com>
2022-08-23 11:46:38 +0000

    Tue, Aug 23, 2022, 11:46 AM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -7,22 +7,17 @@ while True:
             print("Too few command-line arguments")
         elif len(sys.argv) > 2:
             print("Too many command-line arguments")
-        elif sys.argv.endswith(".py") == False:
+        elif sys.argv[1].endswith(".py") == False:
             print("Not a Python file")
         else:
             i = 0
             with open(sys.argv[1]) as file:
                 for line in file:
                     i += 1
+                print(i)
     except FileNotFoundError:
         sys.exit("File does not exist")
 
-i = 0
-
-#with open(sys.argv[1]) as file:
-
-    #for line in file:
-        #i += 1
 
 
 #print(i)
