4dccd87e3e6005bea0abdebe7fcf5c69c95c4350
N3rg <moren.g.couch@gmail.com>
2022-08-23 11:48:22 +0000

    Tue, Aug 23, 2022, 11:48 AM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -4,11 +4,11 @@ import sys
 while True:
     try:
         if len(sys.argv) == 1:
-            print("Too few command-line arguments")
+            sys.exit("Too few command-line arguments")
         elif len(sys.argv) > 2:
-            print("Too many command-line arguments")
+            sys.exit("Too many command-line arguments")
         elif sys.argv[1].endswith(".py") == False:
-            print("Not a Python file")
+            sys.exit("Not a Python file")
         else:
             i = 0
             with open(sys.argv[1]) as file:
