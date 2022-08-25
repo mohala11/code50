3c4708437e4a35612176854c89fc09565c4eaa6b
N3rg <moren.g.couch@gmail.com>
2022-08-25 11:03:39 +0000

    Thu, Aug 25, 2022, 11:03 AM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -7,22 +7,19 @@
 import sys
 
 
-while True:
-    try:
-        if len(sys.argv) == 1:
-            sys.exit("Too few command-line arguments")
-        elif len(sys.argv) > 2:
-            sys.exit("Too many command-line arguments")
-        elif sys.argv[1].endswith(".py") == False:
-            sys.exit("Not a Python file")
-        else:
-            i = 0
-            with open(sys.argv[1]) as file:
-                for line in file:
-                    if not line.lstrip(" ").startswith("#") and not line.isspace():
-                        i += 1
-    except FileNotFoundError:
-        sys.exit("File does not exist")
-    finally:
-        print(i)
-        break
+try:
+    if len(sys.argv) == 1:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    elif sys.argv[1].endswith(".py") == False:
+        sys.exit("Not a Python file")
+    else:
+        i = 0
+        with open(sys.argv[1]) as file:
+            for line in file:
+                if not line.lstrip(" ").startswith("#") and not line.isspace():
+                    i += 1
+except FileNotFoundError:
+    sys.exit("File does not exist")
+print(i)
--- pizza/pizza.py
+++ pizza/pizza.py
@@ -2,22 +2,22 @@ from tabulate import tabulate
 import sys
 import csv
 
-while True:
-    try:
-        if len(sys.argv) == 1:
-            sys.exit("Too few command-line arguments")
-        elif len(sys.argv) > 2:
-            sys.exit("Too many command-line arguments")
-        elif sys.argv[1].endswith(".csv") == False:
-            sys.exit("Not a Python file")
-        else:
-            pizza = []
-            with open(sys.argv[1]) as file:
-                reader = csv.reader(file)
-                for row in reader:
-                    pizza.append(row)
-    except FileNotFoundError:
-        sys.exit("File does not exist")
-    print(tabulate(pizza[1:], headers=pizza[0], tablefmt="grid"))
+try:
+    if len(sys.argv) == 1:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    elif sys.argv[1].endswith(".csv") == False:
+        sys.exit("Not a Python file")
+    else:
+        pizza = []
+        with open(sys.argv[1]) as file:
+            reader = csv.reader(file)
+            for row in reader:
+                pizza.append(row)
+except FileNotFoundError:
+    sys.exit("File does not exist")
+print(tabulate(pizza[1:], headers=pizza[0], tablefmt="grid"))
+
 
 
