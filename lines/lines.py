f161eff1824928ac1fb6a340042de298e32f943d
N3rg <moren.g.couch@gmail.com>
2022-08-25 10:07:32 +0000

    Thu, Aug 25, 2022, 10:07 AM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,5 +1,6 @@
+#In a file called lines.py, implement a program that expects exactly one command-line argument, the name (or path) of a Python file, and outputs the number of lines of code in that file, excluding comments and blank lines. If the user does not specify exactly one command-line argument, or if the specified file’s name does not end in .py, or if the specified file does not exist, the program should instead exit via sys.exit.
 
-
+#Assume that any line that starts with #, optionally preceded by whitespace, is a comment. (A docstring should not be considered a comment.) Assume that any line that only contains whitespace is blank.
 
 
 
@@ -29,5 +30,3 @@ while True:
     finally:
         print(i)
         break
-
-
