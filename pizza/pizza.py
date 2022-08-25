4a02f9303b791c1526f3b6ecc92895746778a812
N3rg <moren.g.couch@gmail.com>
2022-08-25 10:23:18 +0000

    Thu, Aug 25, 2022, 10:23 AM +00:00

---
+++ pizza/pizza.py
@@ -0,0 +1,19 @@
+import tabulate
+import sys
+
+while True:
+    try:
+        if len(sys.argv) == 1:
+            sys.exit("Too few command-line arguments")
+        elif len(sys.argv) > 2:
+            sys.exit("Too many command-line arguments")
+        elif sys.argv[1].endswith(".csv") == False:
+            sys.exit("Not a Python file")
+        else:
+            with open(sys.argv[1]) as file:
+                print(tabulate(table, headers, tablefmt="grid"))
+                break
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+
+
