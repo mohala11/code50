1879565176b67f6032b8a4f3f2056a896c8a695b
N3rg <moren.g.couch@gmail.com>
2022-08-25 11:07:41 +0000

    Thu, Aug 25, 2022, 11:07 AM +00:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -2,22 +2,33 @@ from tabulate import tabulate
 import sys
 import csv
 
-try:
-    if len(sys.argv) == 1:
-        sys.exit("Too few command-line arguments")
-    elif len(sys.argv) > 2:
-        sys.exit("Too many command-line arguments")
-    elif sys.argv[1].endswith(".csv") == False:
-        sys.exit("Not a Python file")
+
+def main():
+    check_cvs_file()
+    try:
+        
+
+
+
+
+def check_cvs_file():
+    try:
+        if len(sys.argv) == 1:
+            sys.exit("Too few command-line arguments")
+        elif len(sys.argv) > 2:
+            sys.exit("Too many command-line arguments")
+        elif sys.argv[1].endswith(".csv") == False:
+            sys.exit("Not a Python file")
+
+
+
+
+
     else:
-        pizza = []
-        with open(sys.argv[1]) as file:
-            reader = csv.reader(file)
-            for row in reader:
-                pizza.append(row)
+
 except FileNotFoundError:
     sys.exit("File does not exist")
-print(tabulate(pizza[1:], headers=pizza[0], tablefmt="grid"))
+
 
 
 
