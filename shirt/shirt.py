3cba6f79f38ee507fdf921419ad72c172e520dfa
N3rg <moren.g.couch@gmail.com>
2022-08-31 10:08:55 +0000

    Wed, Aug 31, 2022, 10:08 AM +00:00

---
+++ shirt/shirt.py
@@ -0,0 +1,15 @@
+import sys
+from PIL import Image
+
+def main():
+    check_image_file()
+
+
+
+def check_image_file():
+        if len(sys.argv) < 2:
+            sys.exit("Too few command-line arguments")
+        elif len(sys.argv) > 3:
+            sys.exit("Too many command-line arguments")
+        elif sys.argv[1].endswith() and sys.argv[2].endswith(".csv") == False:
+            sys.exit("Not a CSV file")
