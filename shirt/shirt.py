3658179201b6f5f0751650663dbbca7fcc6f6cd1
N3rg <moren.g.couch@gmail.com>
2022-08-31 10:19:42 +0000

    Wed, Aug 31, 2022, 10:19 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,5 +1,6 @@
 import sys
 from PIL import Image
+import pathlib
 
 def main():
     check_image_file()
@@ -12,6 +13,8 @@ def check_image_file():
             sys.exit("Too many command-line arguments")
         elif not sys.argv[1-2].lower().endswith(".jpg") or sys.argv[1-2].lower().endswith(".jpeg") or sys.argv[1-2].lower().endswith(".png"):
             sys.exit("Invalid input")
+        elif pathlib.Path(sys.argv[1].lower()).suffix != pathlib.Path(sys.argv[2].lower()).suffix:
+            sys.exit("Input and output have different extensions")
 
 
 main()
