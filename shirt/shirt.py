7cd34946d2d053adb5f91b0e8a5f2c2cdd4c00d0
N3rg <moren.g.couch@gmail.com>
2022-08-31 10:17:07 +0000

    Wed, Aug 31, 2022, 10:17 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -5,14 +5,13 @@ def main():
     check_image_file()
 
 
-
 def check_image_file():
         if len(sys.argv) < 2:
             sys.exit("Too few command-line arguments")
         elif len(sys.argv) > 3:
             sys.exit("Too many command-line arguments")
         elif not sys.argv[1-2].lower().endswith(".jpg") or sys.argv[1-2].lower().endswith(".jpeg") or sys.argv[1-2].lower().endswith(".png"):
-            sys.exit("пошел нахуй")
+            sys.exit("Invalid input")
 
 
 main()
