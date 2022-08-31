387e4dc04885d6ac7ab6d4ae85a157b2217098a4
N3rg <moren.g.couch@gmail.com>
2022-08-31 11:50:20 +0000

    Wed, Aug 31, 2022, 11:50 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -4,17 +4,18 @@ import pathlib
 
 def main():
     check_image_file()
-    print(sys.argv[1][-4:].lower())
 
 
 def check_image_file():
         if len(sys.argv) < 3:
             sys.exit("Too few command-line arguments")
-        elif len(sys.argv) > 3:
+        if len(sys.argv) > 3:
             sys.exit("Too many command-line arguments")
-        elif sys.argv[1][-4:].lower() != ".jpg" or sys.argv[1][-4:].lower() != ".jpeg" or sys.argv[1][-4:].lower() != ".png":
+        if not sys.argv[1][-4:].lower() in [".jpg", ".jpeg", ".png"]:
             sys.exit("Invalid input")
-        elif pathlib.Path(sys.argv[1].lower()).suffix != pathlib.Path(sys.argv[2].lower()).suffix:
+        if not sys.argv[2][-4:].lower() in [".jpg", ".jpeg", ".png"]:
+            sys.exit("Invalid input")
+        if pathlib.Path(sys.argv[1].lower()).suffix != pathlib.Path(sys.argv[2].lower()).suffix:
             sys.exit("Input and output have different extensions")
 
 
